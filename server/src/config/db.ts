fd748d47a2f29add0c1e1384125ce96725ed7393
NaduniTPerera <naduniperera283@gmail.com>
2026-06-11 12:02:02 +0530

    Update db.ts

--- server/src/config/db.ts
+++ server/src/config/db.ts
@@ -1,10 +1,10 @@
-import { Pool } from 'pg';
-import dotenv from 'dotenv';
-
-dotenv.config();
+import { Pool } from "pg";
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: process.env.NODE_ENV === "production"
+    ? { rejectUnauthorized: false }
+    : false,
 });
 
 export default pool;
